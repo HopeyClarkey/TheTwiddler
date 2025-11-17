9d19a6d13581f8db6dd52e8740bf69de790ca17c
Hope Clarke <thehopeclarke@gmail.com>
2025-11-17 00:40:11 -0600

    clarity clean up

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ $(() => {
   const $page = $('#all-contents');// this variable represents a jQ OBJECT that contains the main content
 // div with the ID all-Contents. This div will be used to hold all the content of the page.
 
-//HEADER AND TWEETS TIMELINE
+//SECTION ONE:~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//HEADER AND TWEETS TIMELINE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
    const $header = $('<header></header>').css({ // okay so this creates a header for my page
     padding: '10px', // it's got a padding of 10px inside the header div
@@ -15,13 +16,12 @@ $(() => {
     borderRadius: '8px', //this rounds my corners!
     border: ' 2px solid lightpink', //adding barbie vibes
     marginBottom: '10px', //this gives us space between other obejcts and the header
-    marginRight: '270px', //my sidebar is 50 pixles, so this gives me a little room here.
-  });
+    marginRight: '270px', });//my sidebar is 50 pixles, so this gives me a little room here.
 
-  $header.text('Welcome to Twiddler!'); //fill header with words
-  $page.prepend($header); //add header to the TOP of the page with Prepend
+$header.text('Welcome to Twiddler!'); //fill header with words
+$page.prepend($header); //add header to the TOP of the page with Prepend
 
-//TWEETS TIMELINE
+//TWEETS TIMELINE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   const $tweetsDiv = $('<div id ="tweets" class="tweets"></div>').css({ //gotta create a box for the newTweets, match the others
     padding: '10px', //padding
     marginTop: '10px', //space from the tweet button
@@ -31,19 +31,17 @@ $(() => {
     width: 'calc(100% - 270px)', // width
     boxSizing: 'border-box',
     display: 'flex', // this will have div resize with window movement.
-    alignItems: 'stretch' //this will have items resize inside
-  });
+    alignItems: 'stretch' }); //this will have items resize inside
 
   const $tweetsList =$('<div class = "tweet-list"></div>').css({  //I ran into trouble deleting the entire tweetDiv instead of
   // just the tweets when I needed new ones. Added this so I can just go ahead and delete the tweets and not the object itself.
     paddingTop: '30px', //I liked the way this looked best
-    fontSize: '22px' //COME BACK TO THIS
-  });
-  $tweetsDiv.append($tweetsList); //all right, popping that list into the tweets div
+    fontSize: '22px' });
+
+$tweetsDiv.append($tweetsList); //all right, popping that list into the tweets div
 
 
-
-//Write New Tweets button comes next, just creating the section for it:
+//WRITE NEW TWEETS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   const $writeNewTweet =$('<section id = "write-new-tweet"></section>').css({ //creates write new tweet section
     padding: '10px',  //more padding
@@ -55,8 +53,7 @@ $(() => {
     width: 'calc(100% - 270px)', //fullwidth minus sidebar plus margin of 60?
     boxSizing: 'border-box', //okay I learned this when I was having trouble with the sidebar covering up the edges
     display: 'flex', //this creates a dynamic display of the div that will resize!
-    alignItems: 'stretch' //I want the text input boxes to move with the box.
-  })
+    alignItems: 'stretch' });//I want the text input boxes to move with the box.
 
 //create input text box
   const $tweetText = $('<input id="message-input" type="text" placeholder="Give us the Twiddle!" maxlength="280"/>').css({ //input text for tweets
@@ -69,8 +66,7 @@ $(() => {
     color: 'teal',
     borderRadius: '8px', //this rounds my corners!
     border: ' 2px solid lightpink', //adding barbie vibes
-    cursor: 'text' //this changes it to the text input!
-  });
+    cursor: 'text'}); //this changes it to the text input!
 
   //create input button
   const $tweetSubmit = $('<input type= "submit" value = "Tweet">').css({
@@ -82,11 +78,10 @@ $(() => {
     backgroundColor: 'lightpink', //this is the background color
     borderRadius: '8px', //this rounds my corners!
     border: ' 2px solid lavender', //adding barbie vibes
-    cursor: 'pointer' // this changes it to a pointer
-  });
+    cursor: 'pointer' });// this changes it to a pointer
 
 //this function adds the tweet to the home array to pass the tests
-    function addTweetToHome(user, message){ //takes in a user and message
+  function addTweetToHome(user, message){ //takes in a user and message
       const newTweet = { //creates new tweet object
         user,
         message,
@@ -96,11 +91,10 @@ $(() => {
       return newTweet;
     }
 
-
-  $writeNewTweet.append($tweetText, $tweetSubmit) //just put the tweet text input box and the button to submit
+$writeNewTweet.append($tweetText, $tweetSubmit) //just put the tweet text input box and the button to submit
   // the tweet in the $writeNewTweet div
 
-  $tweetSubmit.on('click', () => { //on the click of tweet submit
+$tweetSubmit.on('click', () => { //on the click of tweet submit
 
 //Text stuff
         let messageText = $tweetText.val().trim(); //takes the value of the text in the text box and trims any extra space around the message.
@@ -125,12 +119,13 @@ $(() => {
         const $timeStamp = $(`<span class="timestamp">${moment(createdAt).format('MMMM Do YYYY, h:mm:ss a')}</span>`);
         const $humanFriendlyTimeStamp = $(`<span class="humanFriendlyTimestamp">${moment(createdAt).fromNow()}</span>`);
 
-  $tweet.append($user, $message, $timeStamp, $humanFriendlyTimeStamp);
-  $tweetsList.prepend($tweet); //put newtweet to top of tweet list
-  $tweetText.val(''); //clear the tweetbox value
-    });
+$tweet.append($user, $message, $timeStamp, $humanFriendlyTimeStamp);
+$tweetsList.prepend($tweet); //put newtweet to top of tweet list
+$tweetText.val(''); //clear the tweetbox value
+  });
 
-//SIDEBAR STUFF
+//SECTION TWO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//SIDEBAR STUFF~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //MAIN SECTION
   const $sideBar = $('<div id ="sidebar" class ="sidebar"></div>').css({ //okay I wanted a sidebar where I can show things, log in, etc
     position: 'fixed', //I want this sidebar to stay on the right
@@ -143,9 +138,7 @@ $(() => {
     border: ' 2px solid lavender', // tie my design
     overflowY: 'auto', //this means it will overlay things- can cause issues but I wanted it to stay where it is no matter
     // what else is on the page.
-    backgroundColor: 'powderblue'
-  });
-
+    backgroundColor: 'powderblue'});
 
 //Log In Information:
   const $logIn = $('<div class="friends"></div>').css({
@@ -157,8 +150,8 @@ $(() => {
     marginRight: '15px',
     marginLeft: '15px',
     marginTop: '15px',
-    backgroundColor: 'lavender'
-  });
+    backgroundColor: 'lavender'});
+
 //text for the username
   const $logInUser =  $('<input id="username-input" type="text" placeholder="Enter Username">').css({
     top: '10px', //just a little room around it.
@@ -171,8 +164,7 @@ $(() => {
     border: ' 2px solid powderblue',
     width: '200px', //define size
     zIndex: 10,
-    cursor: 'text',
-  });
+    cursor: 'text'});
 
 //log in and clear buttons
   const $logInButton = $('<button>Log In</button>').css({
@@ -187,8 +179,8 @@ $(() => {
     marginTop: '2.5px',
     marginLeft: '15px',
     zIndex: 10,
-    cursor: 'pointer'
-  });
+    cursor: 'pointer'});
+
     const $clearName = $('<button>Clear</button>').css({
     top: '10px', //just a little room around it.
     backgroundColor: 'lightpink', //malibu blue
@@ -200,8 +192,8 @@ $(() => {
     marginRight: '15px',
     marginLeft: '15px',
     zIndex: 10,
-    cursor: 'pointer'
-  });
+    cursor: 'pointer'});
+
 //shows who is logged in
   const $currentName = $('<div class="current-name"></div>').css({
     padding: '5px',
@@ -212,16 +204,15 @@ $(() => {
     marginLeft: '15px',
     marginTop: '15px',
     color: 'teal',
-    alignItems: 'fill',
-  });
+    alignItems: 'fill',});
 
-  $logIn.append($logInUser, $logInButton, $clearName); //add the username, and the two buttons
-  $logIn.append($currentName); //put the current name here C
+$logIn.append($logInUser, $logInButton, $clearName); //add the username, and the two buttons
+$logIn.append($currentName); //put the current name here C
 
 //button stuff to make this all work:
   window.visitor = null; //<<this is the default when someone visits
 
-  $logInButton.on('click', () =>{ // on the log in button click
+$logInButton.on('click', () =>{ // on the log in button click
     const name = $logInUser.val().trim(); //going to be using these a lot, the value of the box, with any extra outside spaces removed
     if (!name) { //if there is no name
       alert("Username required!"); //tell the visitor to add one
@@ -229,10 +220,10 @@ $(() => {
     } //if there is
     window.visitor = name; //this is the reset of the window visitor to the name!
       alert(`Logged in as @${window.visitor}`);
-    $currentName.text(`Logged in as: ${window.visitor}`); //updates the log in title with name!
+  $currentName.text(`Logged in as: ${window.visitor}`); //updates the log in title with name!
   });
 
-  $clearName.on('click', () =>{ //on the clear button click
+$clearName.on('click', () =>{ //on the clear button click
     window.visitor =null; //clear the window visitor
     $logInUser.val(''); //this is so freaking useful clear the value
     $currentName.text('Not Logged In'); //update to not logged in.
@@ -261,16 +252,14 @@ $(() => {
     marginTop: '15px',
     backgroundColor: 'lavender'
   });
-  $sideBar.append($logIn,$friendsOfDiv,$otherSection); //put all of these sections in the sidebar!
-
-
-  const $contentContainer = $('<div id = "content-container"></div>').css({ // this overall holds my sidebar on one side, and my tweets div and sidebar, inside the page
+$sideBar.append($logIn,$friendsOfDiv,$otherSection); //put all of these sections in the sidebar!
+  const $contentContainer = $('<div id = "content-container"></div>').css({ // this overall holds my sidebar on one side, and my tweets div and sidebar
     display: 'flex', //insures that the content moves and resizes when the rest of the elements do.
   });
 
-  $contentContainer.append($tweetsDiv, $sideBar);
-  $page.append($writeNewTweet);
-  $page.append($contentContainer)
+$contentContainer.append($tweetsDiv, $sideBar);
+$page.append($writeNewTweet);
+$page.append($contentContainer)
 
 //C
 // allow the user to click on any username to see that users timeline. The user should be able to get back to the home timeline
@@ -281,54 +270,43 @@ $(() => {
 //2- allow the user to click on their own username to see their own timeline
 //3- allow the user to click on a hashtag to see all the tweets with that hashtag in it.
 
+//SECTION THREE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// MORE FUNCTIONALITY IN TWEETS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 //create new tweets button
-const $newTweetsButton = $('<button id="new-tweets-button">Show New Tweets!</button>').css({
+  const $newTweetsButton = $('<button id="new-tweets-button">Show New Tweets!</button>').css({
   //okay so this creates the $jQ that creates the new button
-  position: 'absolute', //this means it wont move around the page
-  top: '10px', //just a little room around it.
-  backgroundColor: 'powderblue', //malibu blue
-  borderRadius: '8px', //round corners
-  border: ' 2px solid lavender', //tie together
-  right: '5px',
-  marginRight: '15px',
-  marginLeft: '15px',
-  zIndex: 10
-});
+    position: 'absolute', //this means it wont move around the page
+    top: '10px', //just a little room around it.
+    backgroundColor: 'powderblue', //malibu blue
+    borderRadius: '8px', //round corners
+    border: ' 2px solid lavender', //tie together
+    right: '5px',
+    marginRight: '15px',
+    marginLeft: '15px',
+    zIndex: 10
+  });
 $tweetsDiv.css({position: 'relative'});
 $tweetsDiv.prepend($newTweetsButton);
-
-//A
-//Show the new user tweets somehow. You can show them automatically as they are created every 10 seconds, or create a button
-//that displays new tweets: The button should have an id of new-tweets-button. New Tweets should be added to the top of the
-//list of tweets.
-// All tweets in the streams.home array should be displayed. There is a container div that has the class tweets
-//that you can append the tweets to. Each tweet should hav ethe class tweet and should be appended to the tweets container.
-
   function showTweets(tweetArray){
-    $tweetsList.html('')//removes old tweets
+$tweetsList.html('')//removes old tweets
     tweetArray.forEach((tweet) =>{ //loops through tweet Array
       if (!tweet || !tweet.user) return; // if the tweet is undefined, return with nothing.
-
+//create tweet:
       const $tweet = $('<div class="tweet"></div>'); //creates a div for each tweet
       const $user = $(`<span class="username">@${tweet.user}</span>`); //creates a user for ech tweet, with the class username @ template
       const $message = $(`<span class="message">${tweet.message}</span>`); //creates message itself.
-
+//fix timestamp formatting:
       let createdAt = tweet.created_at instanceof Date ? new Date(tweet.created_at): new Date(); //creates variable for date
-
       const momentTimeAgo = moment(createdAt).fromNow() ; //converts with moment how long ago
       const momentTime = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a'); //converts with moment actual time
-
       const $timeStamp = $(`<span class="timestamp"> ${momentTime}</span>`); //creates timestamp
       const $humanFriendlyTimeStamp = $(`<span class="humanFriendlyTimestamp"> ${momentTimeAgo}</span>`);
+$tweet.append($user, $message, $timeStamp, $humanFriendlyTimeStamp); //appends the user, the message, and the time stamp to the tweet
+$tweetsList.prepend($tweet); });//appends the tweet to the tweetDiv
+  };
 
-
-      console.log(tweet);
-    $tweet.append($user, $message, $timeStamp, $humanFriendlyTimeStamp); //appends the user, the message, and the time stamp to the tweet
-    $tweetsList.prepend($tweet); //appends the tweet to the tweetDiv
-
-  });
-  }
 
 showTweets(streams.home); //calls the function we just made.
 
